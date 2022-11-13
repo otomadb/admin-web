5debaf4b11f99fbc1334e6407a3fc23c56fd9f5c
SnO2WMaN <me@sno2wman.net>
2022-11-13 16:03:48 +0900

    init

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    zIndex: {
+      0: 0,
+      1: 1,
+      infinity: 2147483647,
+    },
+    extend: {
+      colors: {
+        character: colors.green,
+        class: colors.violet,
+        music: colors.cyan,
+        work: colors.pink,
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import pluginReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
+import pluginTSconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  publicDir: "public",
+  plugins: [
+    pluginReact(),
+    pluginTSconfigPaths(),
+  ],
+});
