44e1de98dac427bbad96ec8ae68ed0b250288b74
SnO2WMaN <me@sno2wman.net>
2022-11-13 20:05:23 +0900

    Fix tailwindcss setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     zIndex: {
